d60db6f64bb73af771820e8659e40a6e3b20f675
visicool <00vishalvaibhav@gmail.com>
2025-03-02 16:29:05 +0530

    updated dockerfile and test

--- lambdas/remove_pii-s3/src/test/test_handler.py
+++ lambdas/remove_pii-s3/src/test/test_handler.py
@@ -6,13 +6,10 @@ import sys
 # Ensure the module is correctly imported
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from src.remove_pii import (
-    generate_random_id,
-    start_transcription_job,
-    get_transcription_job_status,
-    check_transcription_status,
-    lambda_handler
-)
+from src import remove_pii as handler
+
+
+
 
 class TestRemovePII(unittest.TestCase):
 
@@ -20,7 +17,7 @@ class TestRemovePII(unittest.TestCase):
     @patch('uuid.uuid4')
     def test_generate_random_id_success(self, mock_uuid):
         mock_uuid.return_value = 'abcde123-4567-89ab-cdef-0123456789ab'
-        random_id = generate_random_id()
+        random_id = handler.generate_random_id()
         self.assertEqual(random_id, 'abcde123-4567-89ab-cdef-0123456789ab')
 
     #Test case for generate_random_id function failure
@@ -28,7 +25,7 @@ class TestRemovePII(unittest.TestCase):
     def test_generate_random_id_failure(self, mock_uuid):
         mock_uuid.side_effect = Exception('Error generating random id')
         with self.assertRaises(Exception) as context:
-            generate_random_id()
+            handler.generate_random_id()
         self.assertEqual(str(context.exception), "Error generating random id")
 
     #Test case for start_transcription_job function success
@@ -36,7 +33,7 @@ class TestRemovePII(unittest.TestCase):
     def test_start_transcription_job_success(self, mock_transcribe_client):
         expected_response = {'TranscriptionJob': {'TranscriptionJobName': 'test-job', 'TranscriptionJobStatus': 'IN_PROGRESS'}}
         mock_transcribe_client.start_transcription_job.return_value = expected_response
-        response = start_transcription_job('test-job', 's3://source-bucket/test.mp3', 'target-bucket')
+        response = handler.start_transcription_job('test-job', 's3://source-bucket/test.mp3', 'target-bucket')
         self.assertEqual(response, expected_response)
 
     #Test case for start_transcription_job function failure
@@ -45,7 +42,7 @@ class TestRemovePII(unittest.TestCase):
         expected_response_failure = 'Transcription job failed'
         mock_transcribe_client.start_transcription_job.side_effect = Exception(expected_response_failure)
         with self.assertRaises(Exception) as context:
-            start_transcription_job('test-job', 's3://source-bucket/test.mp3', 'target-bucket')
+            handler.start_transcription_job('test-job', 's3://source-bucket/test.mp3', 'target-bucket')
         self.assertEqual(str(context.exception), expected_response_failure)
 
     #Test case for get_transcription_job_status function success
@@ -53,7 +50,7 @@ class TestRemovePII(unittest.TestCase):
     def test_get_transcription_job_status_success(self, mock_transcribe_client):
         expected_response = {'TranscriptionJob': {'TranscriptionJobName': 'test-job', 'TranscriptionJobStatus': 'IN_PROGRESS'}}
         mock_transcribe_client.get_transcription_job.return_value = expected_response
-        result = get_transcription_job_status('test-job')
+        result = handler.get_transcription_job_status('test-job')
         self.assertEqual(result, expected_response)
 
     #Test case for get_transcription_job_status function failure
@@ -62,7 +59,7 @@ class TestRemovePII(unittest.TestCase):
         expected_response_failure = 'Transcription job failed'
         mock_transcribe_client.get_transcription_job.side_effect = Exception(expected_response_failure)
         with self.assertRaises(Exception) as context:
-            get_transcription_job_status('test-job')
+            handler.get_transcription_job_status('test-job')
         self.assertEqual(str(context.exception), expected_response_failure)
 
     #Test case for check_transcription_status function success
@@ -70,7 +67,7 @@ class TestRemovePII(unittest.TestCase):
     def test_check_transcription_status_success(self, mock_transcribe_client):
         expected_response = {'TranscriptionJob': {'TranscriptionJobName': 'test-job', 'TranscriptionJobStatus': 'COMPLETED'}}
         mock_transcribe_client.get_transcription_job.return_value = expected_response
-        result = check_transcription_status('test-job')
+        result = handler.check_transcription_status('test-job')
         self.assertEqual(result, 'COMPLETED')
 
     #Test case for check_transcription_status function failed
@@ -78,7 +75,7 @@ class TestRemovePII(unittest.TestCase):
     def test_check_transcription_status_failed(self, mock_transcribe_client):
         expected_response = {'TranscriptionJob': {'TranscriptionJobName': 'test-job', 'TranscriptionJobStatus': 'FAILED'}}
         mock_transcribe_client.get_transcription_job.return_value = expected_response
-        result = check_transcription_status('test-job')
+        result = handler.check_transcription_status('test-job')
         self.assertEqual(result, 'FAILED')
 
     #Test case for check_transcription_status function unknown
@@ -86,7 +83,7 @@ class TestRemovePII(unittest.TestCase):
     def test_check_transcription_status_unknown(self, mock_transcribe_client):
         expected_response = {'TranscriptionJob': {'TranscriptionJobName': 'test-job','TranscriptionJobStatus': ''}}
         mock_transcribe_client.get_transcription_job.return_value = expected_response
-        result = check_transcription_status('test-job')
+        result = handler.check_transcription_status('test-job')
         self.assertEqual(result, 'UNKNOWN')
 
     #Test case for check_transcription_status function when it transitions from IN_PROGRESS to COMPLETED
@@ -99,7 +96,7 @@ class TestRemovePII(unittest.TestCase):
             {'TranscriptionJob': {'TranscriptionJobName': 'test-job', 'TranscriptionJobStatus': 'COMPLETED'}}
         ]
         mock_get_transcription_job_status.side_effect = lambda job_name: expected_response.pop(0)
-        result = check_transcription_status('test-job')
+        result = handler.check_transcription_status('test-job')
         self.assertEqual(result, 'COMPLETED')
 
     #Test case for check_transcription_status function failure
@@ -108,9 +105,10 @@ class TestRemovePII(unittest.TestCase):
         expected_response_failure = 'Transcription job failed'
         mock_transcribe_client.get_transcription_job.side_effect = Exception(expected_response_failure)
         with self.assertRaises(Exception) as context:
-            check_transcription_status('test-job')
+            handler.check_transcription_status('test-job')
         self.assertEqual(str(context.exception), expected_response_failure)
 
+    #test case for lambda handler function
 
 if __name__ == '__main__':
     unittest.main()
