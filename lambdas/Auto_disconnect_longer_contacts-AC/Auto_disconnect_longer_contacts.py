54fbadf58b81e335b40bd42e1d26148944ce9609
visicool <00vishalvaibhav@gmail.com>
2025-03-06 10:12:20 +0530

    update s3

--- lambdas/Auto_disconnect_longer_contacts-AC/Auto_disconnect_longer_contacts.py
+++
@@ -1,1 +0,0 @@
-#lambda function to Auto disconnect Contacts older then 2 hour
--- lambdas/hello/src/hello.py
+++ lambdas/hello/src/hello.py
@@ -2,6 +2,7 @@ from common.logger import Logger
 
 # Initialize the custom logger
 logger = Logger()
+LOGGER = Logger(__name__)
 
 def lambda_handler(event, context):
     try:
